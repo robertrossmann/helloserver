0dcd3347f7e7f4c07119c84cf1b6680992450dd2
Robert Rossmann <rr.rossmann@me.com>
2024-07-21 17:26:24 +0200

    chore: refactor `environ.PackageRoot()`

--- environ/environ.go
+++ environ/environ.go
@@ -1,6 +1,7 @@
 package environ
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -10,43 +11,43 @@ var (
 	packageroot string
 )
 
-func findpkgroot() string {
-	_, here, _, ok := runtime.Caller(0)
-	if !ok {
-		panic("runtime.Caller is unavailable")
-	}
-	dirpath := filepath.Dir(here)
-
-	for {
-		candidate := filepath.Join(dirpath, "go.mod")
-		isRootDirectory := dirpath == filepath.Dir(dirpath)
-
-		// go.mod is not present, use current working directory
-		if isRootDirectory {
-			pwd, err := os.Getwd()
-			if err != nil {
-				panic(err)
-			}
-
-			return pwd
-		}
-
-		_, err := os.Stat(candidate)
-		if err == nil {
-			break
-		} else {
-			dirpath = filepath.Dir(dirpath)
-		}
-	}
-
-	return dirpath
-}
-
 func init() {
 	packageroot = findpkgroot()
 }
 
-// PackageRoot returns the nearest directory with a go.mod file. Generally that is the root of the package.
+func findpkgroot() string {
+	_, current, _, ok := runtime.Caller(0)
+	if !ok {
+		return getwd()
+	}
+
+	for {
+		dir := filepath.Dir(current)
+		defer func() { current = dir }()
+
+		isRoot := dir == filepath.Dir(dir)
+		_, err := os.Stat(filepath.Join(dir, "go.mod"))
+		// ✅ Found the directory with go.mod
+		if err == nil {
+			return dir
+		}
+
+		if isRoot {
+			return getwd()
+		}
+	}
+}
+
+func getwd() string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(fmt.Errorf("os.Getwd: %w", err))
+	}
+
+	return pwd
+}
+
+// PackageRoot returns the nearest directory with a go.mod file. Generally that is the root of the package/project.
 func PackageRoot() string {
 	return packageroot
 }
